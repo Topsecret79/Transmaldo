b77764ffdf50cecb1ef1f2cc053612c68d11d06e
Topsecret79 <topsecret79@example.com>
2026-06-30 14:04:37 +0200

    BumpSWCacheVersionToV7

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 // sw.js - Service Worker optimizado (Network-First) para recibir actualizaciones al instante
-const CACHE_NAME = 'delivery-app-v6';
+const CACHE_NAME = 'delivery-app-v7';
 const ASSETS = [
   '/',
   '/index.html',
